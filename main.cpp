7d291fb2651de555f85644009db3bc55409874a6
artur <babalasoft@gmail.com>
2018-09-29 21:03:56 +0200

    add comments how threads are syncrhonized

--- main.cpp
+++ main.cpp
@@ -11,30 +11,31 @@ using namespace std;
 #define LOG(x) do {cout << hex <<"["<< this_thread::get_id() <<"] " << x << endl;} while (0)
 
 struct thread_data{
-    mutex mMutex;
-    condition_variable mCondVar;
-    vector<string> mDataVector;
+    mutex mMutex;                       //mutex to protect concurrent access to share data
+    condition_variable mCondVar;        //condition variable to wait / notify
+    vector<string> mDataVector;         //data which is shared between threads
 };
 class Consumer{
 public:
     Consumer(thread_data* p) : mData(p){};
     inline void operator()(){
         if(mData){
-            unique_lock<mutex> cout_lck(mData->mMutex);
+            //this lock is needed to sync output to cout
+            unique_lock<mutex> cout_lck(mData->mMutex); //mutex is locked here
             
             LOG("Consumer thread entering execution");
             
-            cout_lck.unlock();
+            cout_lck.unlock();                          //unlock the mutex
             
             while(1){
-                unique_lock<mutex> lck(mData->mMutex);
-                mData->mCondVar.wait(lck);
-                if(mData->mDataVector.size()>0){
+                unique_lock<mutex> lck(mData->mMutex); //mutex is locked here
+                mData->mCondVar.wait(lck);             //mutex will be unlocked while waiting   
+                if(mData->mDataVector.size()>0){       //and locked again after thread is leaving waiting
                     LOG("Consumer: " << mData->mDataVector.back());
                     mData->mDataVector.pop_back();
                 }
-                lck.unlock();
-                this_thread::sleep_for(chrono::seconds(3));
+                lck.unlock();                               //unlock thread here
+                this_thread::sleep_for(chrono::seconds(3)); //sleep just to simulate run time of a thread
             }
         }
     };
@@ -44,28 +45,29 @@ private:
 
 void consumer(thread_data* pSharedData){
     if(pSharedData){
-        
-        unique_lock<mutex> cout_lck(pSharedData->mMutex);
+        //this lock is needed to sync output to cout
+        unique_lock<mutex> cout_lck(pSharedData->mMutex); //mutex is locked here
         
         LOG("consumer thread entering execution");
         
-        cout_lck.unlock();
+        cout_lck.unlock();                              //unlock the mutex
         
          while(1){
-            unique_lock<mutex> lck(pSharedData->mMutex);
-            pSharedData->mCondVar.wait(lck);
-            if(pSharedData->mDataVector.size()>0){
+            unique_lock<mutex> lck(pSharedData->mMutex);    //mutex is locked here
+            pSharedData->mCondVar.wait(lck);                //mutex will be unlocked while waiting
+            if(pSharedData->mDataVector.size()>0){          //and locked again after thread is leaving waiting
                 LOG("consumer: " << pSharedData->mDataVector.back());
                 pSharedData->mDataVector.pop_back();
             }
-            lck.unlock();
-            this_thread::sleep_for(chrono::seconds(1));
+            lck.unlock();                                   //unlock thread here
+            this_thread::sleep_for(chrono::seconds(1));     //sleep just to simulate run time of a thread
         }
     }
 } 
 
 void producer(thread_data* pSharedData){
     if(pSharedData){
+        //this lock is needed to sync output to cout
         unique_lock<mutex> cout_lck(pSharedData->mMutex);
         
         LOG("producer thread entering execution");
@@ -73,15 +75,15 @@ void producer(thread_data* pSharedData){
         cout_lck.unlock();
         
         while(1){
-            unique_lock<mutex> lck(pSharedData->mMutex);
+            unique_lock<mutex> lck(pSharedData->mMutex); //lock to be able to write to data
             
             pSharedData->mDataVector.push_back("Hello");
             pSharedData->mDataVector.push_back("World!");
             
             LOG("producer: " << pSharedData->mDataVector.size() << " elements in queue");
             
-            pSharedData->mCondVar.notify_all();
-            lck.unlock();
+            pSharedData->mCondVar.notify_all(); //notify all waiting threads
+            lck.unlock();                       //unlock the mutex to allow other threads to be able to access the data
             
             this_thread::sleep_for(chrono::seconds(3));
         }
